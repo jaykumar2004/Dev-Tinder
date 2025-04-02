c0d68f819b555d4d1a97b27a644e62c7d735eb53
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2025-04-02 15:18:08 +0530

    exploring complex routing

--- src/app.js
+++ src/app.js
@@ -7,29 +7,29 @@ const app = express();
 
 //handle the code
 
-
-app.use("/user",(req,res)=>{
-    res.send("HAHAHAHAHH")
-}); //this gives "HHAHAHHAHA" because order matters
-
 //this will only handle get calls to /user  
-app.get("/user",(req,res)=>{
-    res.send({firstName : "Jay", lastName : "Kumar"})
+app.use("/user",(res,req)=>{
+	console.log(req.query) // {userId:101}
+	res.send("Hello welcome to the server")
 })
 
-app.post("/user",(req,res)=>{
-    //sacing data to DB
-    res.send("Data successfully saved to database!");
+// getting the user id from the route /user?userid=101 and /user?userid=101&password=hello
+app.use("/user",(res,req)=>{
+	console.log(req.query) // {userId:101,password:hello}
+	res.send("Hello welcome to the server")
 })
 
-app.delete("/user",(req,res)=>{
-    res.send("Deleted successfully!!!")
+// getting the user id from the route /user/707 ':'represents dynamic routing
+app.use("/user/:userId",(res,req)=>{
+	console.log(req.params) // {userId:707}
+	res.send("Hello welcome to the server")
 })
 
-//this will match the all http mathod API calls to /test
-app.use("/test",(req,res)=>{
-    res.send("hello from the server")
-});
+// getting the user id from the route /user/707/satyam/hello ':'represents dynamic routing
+app.use("/user/:userId/:name/:password",(res,req)=>{
+	console.log(req.params) // {userId:707,name:satyam,password:hello}
+	res.send("Hello welcome to the server")
+})
 
 
 app.listen(7000,()=>{
