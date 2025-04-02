71e0d9358088e51b11bd6a1522f54624b88ec5aa
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2025-04-02 22:40:35 +0530

    more about routing

--- src/app.js
+++ src/app.js
@@ -5,33 +5,11 @@ const express = require('express');
 const app = express();
 
 
-//handle the code
-
-//this will only handle get calls to /user  
-app.use("/user",(res,req)=>{
-	console.log(req.query) // {userId:101}
-	res.send("Hello welcome to the server")
-})
-
-// getting the user id from the route /user?userid=101 and /user?userid=101&password=hello
-app.use("/user",(res,req)=>{
-	console.log(req.query) // {userId:101,password:hello}
-	res.send("Hello welcome to the server")
-})
-
-// getting the user id from the route /user/707 ':'represents dynamic routing
-app.use("/user/:userId",(res,req)=>{
-	console.log(req.params) // {userId:707}
-	res.send("Hello welcome to the server")
-})
-
-// getting the user id from the route /user/707/satyam/hello ':'represents dynamic routing
-app.use("/user/:userId/:name/:password",(res,req)=>{
-	console.log(req.params) // {userId:707,name:satyam,password:hello}
-	res.send("Hello welcome to the server")
-})
+app.use("/user",(req,res)=>{
+    // res.send("route handler 1")
+});
 
 
-app.listen(7000,()=>{
+app.listen(7000,()=>{ 
     console.log("server is successfully listining on port 7000...");
 });
