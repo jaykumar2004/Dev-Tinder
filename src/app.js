acbb0b8f9182137487dc2bd6bc6dde4521648ef8
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2025-04-03 23:12:23 +0530

    middlewares

--- src/app.js
+++ src/app.js
@@ -4,10 +4,23 @@ const express = require('express');
 //then create a new application of express
 const app = express();
 
+const {adminAuth} = require("./middlewares/auth")
 
-app.use("/user",(req,res)=>{
-    // res.send("route handler 1")
-});
+// app.use("/admin",adminAuth);
+
+
+// app.get("/user",(req,res)=>{
+//     res.send("User data Sent")
+// })
+
+// app.get("/admin/getAllData",(req,res)=>{
+//     res.send("Get all the data")
+// })
+
+
+// app.get("/admin/deleteUser",(req,res)=>{
+//     res.send("Deleted a User");
+// })
 
 
 app.listen(7000,()=>{ 
