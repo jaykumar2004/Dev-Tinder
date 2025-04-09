a4ffaa6a050ed34ed9fc8a2c7710d77f384ed811
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2025-04-09 15:55:52 +0530

    login apif

--- src/app.js
+++ src/app.js
@@ -35,6 +35,33 @@ app.post("/signup",async(req,res)=>{
 });
 
 
+//login api
+app.post("/login",async(req,res)=>{
+    try{
+        const{emailId,password} = req.body;
+
+        const user = await User.findOne({ emailId: emailId })
+
+
+        
+        if(!user){
+            throw new Error ("Invalid Credentials")
+        }
+        const isPasswordValid = await bcrypt.compare(password,user.password);
+
+        if(isPasswordValid){
+            res.send("Login Successful!!!")
+        }else{
+            throw new Error("Invalid Credentials");
+        }
+
+    }catch(err){
+        res.status(400).send("ERROR : " + err.message);
+    }
+});
+
+
+
 //  Get user from email
 app.get("/user",async(req,res)=>{
     const userEmail = req.body.emailId;
