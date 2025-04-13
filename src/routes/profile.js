3a29cdf87ca48f453028ed6f49440fcd857cefe8
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2025-04-13 19:44:47 +0530

    profile api

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -35,7 +35,7 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
 });
 
 //forgot password
-profileRouter.post("/profile/change-password", userAuth, async (req, res) => {
+profileRouter.patch("/profile/change-password", userAuth, async (req, res) => {
     const { currentPassword, newPassword } = req.body;
   
     try {
