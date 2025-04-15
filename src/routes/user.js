c04b7b02e69c02dd071378f74f4b91f06197b896
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2025-04-16 00:49:23 +0530

    feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -53,16 +53,13 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
 //feed api
 userRouter.get("/feed", userAuth, async (req, res) => {
   try {
-
     const loggedInUser = req.user;
 
     const page = parseInt(req.query.page) || 1;
     let limit = parseInt(req.query.limit) || 10;
 
     limit = limit > 50 ? 50 : limit;
-    const skip = (page-1)*limit;
-
-
+    const skip = (page - 1) * limit;
 
     const connectionRequests = await ConnectionRequest.find({
       $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
@@ -79,9 +76,12 @@ userRouter.get("/feed", userAuth, async (req, res) => {
         { _id: { $nin: Array.from(hideUserFromFeed) } },
         { _id: { $ne: loggedInUser._id } },
       ],
-    }).select(USER_SAFE_DATA).skip(skip).limit(limit);
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
 
-    res.send(users);
+    res.json({data: users});
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
