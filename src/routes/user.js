b65accff6855891c6e07b566f20230bdb82380df
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2025-04-16 00:46:12 +0530

    feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -38,15 +38,52 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
       .populate("toUserId", USER_SAFE_DATA);
 
     const data = connectionRequests.map((row) => {
-        if(row.fromUserId._id.toString() === loggedInUser._id.toString()){
-            return row.toUserId
-        }
-        return row.fromUserId;
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId;
+      }
+      return row.fromUserId;
     });
 
     res.json({ data });
   } catch (err) {
     res.status(404).send({ message: err.message });
+  }
+});
+
+//feed api
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+
+    const loggedInUser = req.user;
+
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page-1)*limit;
+
+
+
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+
+    const hideUserFromFeed = new Set();
+    connectionRequests.forEach((req) => {
+      hideUserFromFeed.add(req.fromUserId.toString());
+      hideUserFromFeed.add(req.toUserId.toString());
+    });
+
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUserFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    }).select(USER_SAFE_DATA).skip(skip).limit(limit);
+
+    res.send(users);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
   }
 });
 
