ea3666cd58975431ecf0bc3a5c247d5d4c63b2df
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2025-04-16 22:41:25 +0530

    added cors

--- src/app.js
+++ src/app.js
@@ -5,7 +5,10 @@ const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
 
-app.use(cors());
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser())
 
