e4047b096e44339884c5622ad4fe5ec98806d0ae
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2025-04-20 15:13:02 +0530

    database error handled

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
-const connectDb = async()=>{
-    await mongoose.connect(
-        "mongodb+srv://jaykumar_2004:QU6AgFgJLkhnm2uI@namastenode.f1wdwtr.mongodb.net/devTinder"
-    );
-}
+const connectDb = async () => {
+    try {
+        await mongoose.connect("mongodb+srv://jaykumar_2004:QU6AgFgJLkhnm2uI@namastenode.f1wdwtr.mongodb.net/");
+        console.log("MongoDB connected successfully!");
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+        throw error;
+    }
+};
 
 module.exports = connectDb;
-
