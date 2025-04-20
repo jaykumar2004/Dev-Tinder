f3b9384dac8500bf2308bf52d4f1f3f2d0101ffd
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2025-04-21 01:01:56 +0530

    added new mongodb url

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
     try {
-        await mongoose.connect("mongodb+srv://jaykumar_2004:QU6AgFgJLkhnm2uI@namastenode.f1wdwtr.mongodb.net/");
+        await mongoose.connect("mongodb+srv://jaykumar_2004:qwertyuiop12345@namastenode.f1wdwtr.mongodb.net/devTinder");
         console.log("MongoDB connected successfully!");
     } catch (error) {
         console.error("MongoDB connection error:", error);
