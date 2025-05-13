4cffd3cb889275c04cfb4fb5fdb2bf0f97c34d52
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2025-05-13 23:05:56 +0530

    socket.io

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,4 +1,12 @@
 const socket = require("socket.io");
+const crypto = require("crypto");
+
+const getSecretRoomId = (userId, targetUserId) => {
+  return crypto
+    .createHash("sha256")
+    .update([userId, targetUserId].sort().join("_"))
+    .digest("hex");
+};
 
 const initializeSocket = (server) => {
   const io = socket(server, {
@@ -7,8 +15,16 @@ const initializeSocket = (server) => {
 
   io.on("connection", (socket) => {
     //handle events
-    socket.on("joinChat", () => {});
-    socket.on("sendMessage", () => {});
+    socket.on("joinChat", ({ firstName, userId, targetUserId }) => {
+      const roomId = getSecretRoomId(userId, targetUserId);
+      console.log(firstName + " joined room : ", roomId);
+      socket.join(roomId);
+    });
+    socket.on("sendMessage", ({ firstName, userId, targetUserId, text }) => {
+      const roomId = getSecretRoomId(userId, targetUserId);
+      console.log(firstName + " " + text);
+      io.to(roomId).emit("messageReceived", { firstName, text });
+    });
     socket.on("disconnect", () => {});
   });
 };
